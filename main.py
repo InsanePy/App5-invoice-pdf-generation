517a201429df4f98c4f84764492885153becda87
StudioCrew-design <dev.studiocrew@gmail.com>
2023-06-24 18:40:16 +0530

    Day 26: Extracted date from filename and added to pdf

--- main.py
+++ main.py
@@ -7,10 +7,20 @@ filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
+
     filename = Path(filepath).stem
-    invoice_nr = filename.split("-")[0]
+    invoice_nr, date = filename.split("-")
+    # The above line can be written as below
+    # invoice_nr = filename.split("-")[0]
+    # date = filename.split("-")[1]
+
     pdf.set_font(family="Times", style="B", size=16)
-    pdf.cell(w=50, h=10, txt=f"Invoice nr: {invoice_nr}")
+    pdf.cell(w=50, h=10, txt=f"Invoice nr: {invoice_nr}", ln=1)
+
+    pdf.set_font(family="Times", style="B", size=16)
+    pdf.cell(w=50, h=10, txt=f"Date : {date}",ln=1)
+
     pdf.output(f"PDFs/{filename}.pdf")
