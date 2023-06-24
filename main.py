69f3f81f29e21edb177c00f70f7ca21b8de81073
StudioCrew-design <dev.studiocrew@gmail.com>
2023-06-24 19:25:28 +0530

    Day 26: Added header columns and data columns in table format

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from pathlib import Path
 filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
-    df = pd.read_excel(filepath, sheet_name="Sheet 1")
 
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
@@ -22,5 +21,28 @@ for filepath in filepaths:
 
     pdf.set_font(family="Times", style="B", size=16)
     pdf.cell(w=50, h=10, txt=f"Date : {date}",ln=1)
+
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    columns = df.columns
+
+    # Rename column names into readable format
+    columns_new = [column.replace("_", " ").title() for column in columns]
+
+    # Add header columns
+    pdf.set_font(family="Times", style="B", size=10)
+    pdf.cell(w=30, h=8, txt=columns_new[0], border=1)
+    pdf.cell(w=60, h=8, txt=columns_new[1], border=1)
+    pdf.cell(w=40, h=8, txt=columns_new[2], border=1)
+    pdf.cell(w=30, h=8, txt=columns_new[3], border=1)
+    pdf.cell(w=30, h=8, txt=columns_new[4], border=1, ln=1)
+
+    for index, row in df.iterrows():
+        # Add table columns
+        pdf.set_font(family="Times", style="B", size=10)
+        pdf.cell(w=30, h=8, txt=str(row['product_id']), border=1)
+        pdf.cell(w=60, h=8, txt=str(row['product_name']), border=1)
+        pdf.cell(w=40, h=8, txt=str(row['amount_purchased']), border=1)
+        pdf.cell(w=30, h=8, txt=str(row['price_per_unit']), border=1)
+        pdf.cell(w=30, h=8, txt=str(row['total_price']), border=1, ln=1)
 
     pdf.output(f"PDFs/{filename}.pdf")
